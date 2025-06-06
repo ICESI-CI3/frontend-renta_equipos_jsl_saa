a7a05dd1cde0fa32575646a0742040aa6c0476bb
SebasL07 <sebasgnv0207@gmail.com>
2025-06-05 20:41:04 -0500

    hotfix

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, FormEvent } from 'react';
+import React, { useState, FormEvent } from 'react';
 import Input from '../ui/Input';
 import Button from '../ui/Button';
 import styles from './style/Form.module.css';
